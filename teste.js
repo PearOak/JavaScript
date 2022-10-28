113d40eb9e7278409cae103b7b15bf8dde889626
PearOak <wellpcarv@hotmail.com>
2022-10-28 17:48:05 -0300

    Teste

--- teste.js
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-console.log("Hello, World! :D");
-=======
-console.log("Hello, World!");
->>>>>>> parent of e6625b3 (Revert "Generic file for testing Git")
-=======
-console.log("Hello, World!");
->>>>>>> parent of e6625b3 (Revert "Generic file for testing Git")
