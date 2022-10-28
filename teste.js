f160d8bebe876c1e82badc2c4316feb7adae6f79
PearOak <wellpcarv@hotmail.com>
2022-10-28 17:44:17 -0300

    Fudeu

--- teste.js
+++ teste.js
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 console.log("Hello, World! :D");
+=======
+console.log("Hello, World!");
+>>>>>>> parent of e6625b3 (Revert "Generic file for testing Git")
 =======
 console.log("Hello, World!");
 >>>>>>> parent of e6625b3 (Revert "Generic file for testing Git")
