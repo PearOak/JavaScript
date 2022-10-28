83c5472e5980473b98e1b4b8d1559611281a3df5
PearOak <wellpcarv@hotmail.com>
2022-10-28 18:15:36 -0300

    Revert "Revert "será?""
    This reverts commit 57765ebf38e52705348a4711c1ea16a90956d367.

--- teste.js
+++ teste.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-console.log("Hello, World! :D");
-=======
-console.log("Hello, World!");
->>>>>>> parent of e6625b3 (Revert "Generic file for testing Git")
-=======
-console.log("Hello, World!");
->>>>>>> parent of e6625b3 (Revert "Generic file for testing Git")
+console.log("Teste")
+console.log("Testando")
+console.log("Testado")
