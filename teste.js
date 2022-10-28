8b952e9dad6ee78c87244f399e379b3ee73171d2
PearOak <wellpcarv@hotmail.com>
2022-10-28 18:04:45 -0300

    Cansei

---
+++ teste.js
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+console.log("Hello, World! :D");
+=======
+console.log("Hello, World!");
+>>>>>>> parent of e6625b3 (Revert "Generic file for testing Git")
+=======
+console.log("Hello, World!");
+>>>>>>> parent of e6625b3 (Revert "Generic file for testing Git")
+=======
+console.log("Hello, World! :D");
+>>>>>>> parent of c40cffa (Hmmmm2)
